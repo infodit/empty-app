f352037656ba228f2864f3ba12b197211ded8a66
infodit <informaticaedigitale@gmail.com>
2024-11-09 07:56:49 +0000

    - Create Test Page

--- app/src/index.ts
+++ app/src/index.ts
@@ -5,7 +5,7 @@ import { Prova } from "./prova.js"
 ; 
 (() => {
     //Write your code
-    const prova = new Prova('Ciao')
+    const prova = new Prova('Programma funziona! Cancella il codice dentro index.ts per scrivere il tuo programma')
     new Box()
     //document.body.innerHTML = 'Programma funziona'
 })()
---
+++ test/server.js
@@ -0,0 +1,37 @@
+import { createServer } from 'node:http';
+import { readFile } from 'node:fs';
+import { join, extname } from 'node:path';
+const server = createServer((req, res) => {
+    const filePath = req.url === '/' ? '/test/index.html' : req.url;
+    const fullPath = join(process.cwd(), filePath || '');
+    // Determina il tipo di contenuto in base all'estensione del file
+    const ext = extname(fullPath);
+    let contentType = 'text/plain';
+    if (ext === '.html')
+        contentType = 'text/html';
+    else if (ext === '.js')
+        contentType = 'application/javascript';
+    else if (ext === '.css')
+        contentType = 'text/css';
+    else if (ext === '.json')
+        contentType = 'application/json';
+    else if (ext === '.png')
+        contentType = 'image/png';
+    else if (ext === '.jpg' || ext === '.jpeg')
+        contentType = 'image/jpeg';
+    // Legge e invia il file richiesto
+    readFile(fullPath, (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/plain' });
+            res.end('File non trovato');
+        }
+        else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(data);
+        }
+    });
+});
+// Avvia il server
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Listening on 127.0.0.1:3000');
+});
